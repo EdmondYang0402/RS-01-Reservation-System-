2d61f67113a0b9c94e9fa846f01744858142e4a6
EdmondYang0402 <a627635716@gmail.com>
2026-09-17 17:35:48 +0800

    new project

---
+++ src/main/java/com/rs01/Rs01Application.java
@@ -0,0 +1,13 @@
+package com.rs01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rs01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rs01Application.class, args);
+	}
+
+}
